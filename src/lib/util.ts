5077254c2c3f706582201180c4479813aea093c1
nanagami1369 <nanagami1369@gmail.com>
2024-06-08 14:32:35 +0900

    曜日を追加

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,5 +6,6 @@ export const formatZonedInternational = (dateTime: Temporal.ZonedDateTime): stri
 }
 
 export const formatZoned = (dateTime: Temporal.ZonedDateTime): string => {
-    return `${new String(dateTime.year).padStart(4, '0')}/${new String(dateTime.month).padStart(2, '0')}/${new String(dateTime.day).padStart(2, '0')} ${new String(dateTime.hour).padStart(2, '0')}:${new String(dateTime.minute).padStart(2, '0')}:${new String(dateTime.second).padStart(2, '0')}`
+    const dayOfWeekFormat = ['月', '火', '水', '木', '金', '土', '日']
+    return `${new String(dateTime.year).padStart(4, '0')}/${new String(dateTime.month).padStart(2, '0')}/${new String(dateTime.day).padStart(2, '0')} (${dayOfWeekFormat[dateTime.dayOfWeek - 1]}) ${new String(dateTime.hour).padStart(2, '0')}:${new String(dateTime.minute).padStart(2, '0')}:${new String(dateTime.second).padStart(2, '0')}`
 }
